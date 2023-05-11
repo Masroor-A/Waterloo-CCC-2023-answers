66733970fdcb72835b76ab885671dee65168e49b
Masroor Ahmad <76138391+Masroor-A@users.noreply.github.com>
2023-05-10 22:23:35 -0400

    Create J1.py

---
+++ J1.py
@@ -0,0 +1,25 @@
+n = int(input())
+	
+row1 = input().split()
+row2 = input().split()
+		
+total = 0 
+
+for a in range(n):
+	if row1[a] == "1":
+		total += 3
+	if (row2[a] == "1"):
+		total += 3;
+		
+for a in range(n-1):
+	if row1[a] == "1" and row1[a+1] == "1":
+		total -= 2
+	if row2[a] == "1" and row2[a+1] == "1":
+		total -= 2;
+		
+		
+for a in range(0, n, 2):
+	if row1[a] == "1" and row2[a] == "1":
+		total -= 2;
+
+print(total);
