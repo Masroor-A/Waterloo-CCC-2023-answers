5f97b7579effd9e17c4ec3bc4009626fce7b0304
Masroor Ahmad <76138391+Masroor-A@users.noreply.github.com>
2023-05-11 09:05:00 -0400

    Update J4.py

--- J4.py
+++ J4.py
@@ -8,18 +8,18 @@ total = 0
 for a in range(n):
 	if row1[a] == "1":
 		total += 3
-	if (row2[a] == "1"):
-		total += 3;
+	if (row2[a] == "1")
+		total += 3
 		
 for a in range(n-1):
 	if row1[a] == "1" and row1[a+1] == "1":
 		total -= 2
 	if row2[a] == "1" and row2[a+1] == "1":
-		total -= 2;
+		total -= 2
 		
 		
 for a in range(0, n, 2):
 	if row1[a] == "1" and row2[a] == "1":
-		total -= 2;
+		total -= 2
 
-print(total);
+print(total)
